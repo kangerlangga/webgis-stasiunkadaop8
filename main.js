0d0eb5829ef7096eb8b9a7c09e46b123126225ff
Erlangga Lesmana Putra <74886811+kangerlangga@users.noreply.github.com>
2023-04-20 20:11:59 +0700

    Update main.js

--- main.js
+++ main.js
@@ -27,6 +27,25 @@ const staAktif_mr = L.marker([-7.472136600278112, 112.43408780823175]).bindPopup
 const staAktif_tga = L.marker([-7.506650465461253, 112.70814880823227]).bindPopup('Stasiun Tanggulangin').addTo(staAktif);
 const staAktif_pr = L.marker([-7.538126634108086, 112.70166926590365]).bindPopup('Stasiun Porong').addTo(staAktif);
 const staAktif_bg = L.marker([-7.598621588450213, 112.77835072357527]).bindPopup('Stasiun Bangil').addTo(staAktif);
+const staAktif_wn = L.marker([-7.6618790653044915, 112.7549663659056]).bindPopup('Stasiun Wonokerto').addTo(staAktif);
+const staAktif_skj = L.marker([-7.71919862328283, 112.72342480823578]).bindPopup('Stasiun Sukorejo').addTo(staAktif);
+const staAktif_sn = L.marker([-7.754828085865252, 112.72441362357777]).bindPopup('Stasiun Sengon').addTo(staAktif);
+const staAktif_lw = L.marker([-7.836466790750451, 112.6976478082377]).bindPopup('Stasiun Lawang').addTo(staAktif);
+const staAktif_sgs = L.marker([-7.896753214776016, 112.66610162358016]).bindPopup('Stasiun Singosari').addTo(staAktif);
+const staAktif_bmg = L.marker([-7.939736846382421, 112.64493926591015]).bindPopup('Stasiun Blimbing').addTo(staAktif);
+const staAktif_ml = L.marker([-7.97727986678661, 112.63702390824]).bindPopup('Stasiun Malang').addTo(staAktif);
+const staAktif_mlk = L.marker([-7.9946081488017935, 112.63252895495847]).bindPopup('Stasiun Malangkotalama').addTo(staAktif);
+const staAktif_psi = L.marker([-8.069049071845066, 112.5984511082416]).bindPopup('Stasiun Pakisaji').addTo(staAktif);
+const staAktif_kpn = L.marker([-8.131732009825072, 112.57325330824261]).bindPopup('Stasiun Kepanjen').addTo(staAktif);
+const staAktif_nb = L.marker([-8.15161056278889, 112.51680503707823]).bindPopup('Stasiun Ngebruk').addTo(staAktif);
+const staAktif_sbp = L.marker([-8.158055725861908, 112.47839853707836]).bindPopup('Stasiun Sumberpucung').addTo(staAktif);
+const staAktif_pgj = L.marker([-8.156718423583767, 112.42011625057229]).bindPopup('Stasiun Pohgajih').addTo(staAktif);
+const staAktif_ksb = L.marker([-8.148908917784508, 112.36352305241975]).bindPopup('Stasiun Kesamben').addTo(staAktif);
+const staAktif_wg = L.marker([-8.088096027572822, 112.32004326591264]).bindPopup('Stasiun Wlingi').addTo(staAktif);
+const staAktif_mst = L.marker([-7.240753581107323, 112.72669127939272]).bindPopup('Stasiun Mesigit').addTo(staAktif);
+const staAktif_klm = L.marker([-7.2196966176859085, 112.73508847939243]).bindPopup('Stasiun Kalimas').addTo(staAktif);
+const staAktif_sbi = L.marker([-7.248149561740208, 112.73114839658177]).bindPopup('Stasiun Surabaya Pasarturi').addTo(staAktif);
+
 
 const staNonaktif = L.layerGroup();
 const staNonaktif_gng = L.marker([-7.584716676572663, 112.72460973591234], {icon: redIcon}).bindPopup('Ex Stasiun Gununggangsir(GNG)').addTo(staNonaktif);
