ec1dc54d61141eb42b78e47083a08481e3ff44ed
Erlangga Lesmana Putra <74886811+kangerlangga@users.noreply.github.com>
2023-04-20 21:02:54 +0700

    Update main.js

--- main.js
+++ main.js
@@ -45,10 +45,41 @@ const staAktif_wg = L.marker([-8.088096027572822, 112.32004326591264]).bindPopup
 const staAktif_mst = L.marker([-7.240753581107323, 112.72669127939272]).bindPopup('Stasiun Mesigit').addTo(staAktif);
 const staAktif_klm = L.marker([-7.2196966176859085, 112.73508847939243]).bindPopup('Stasiun Kalimas').addTo(staAktif);
 const staAktif_sbi = L.marker([-7.248149561740208, 112.73114839658177]).bindPopup('Stasiun Surabaya Pasarturi').addTo(staAktif);
+const staAktif_tes = L.marker([-7.258743864373199, 112.68699607939315]).bindPopup('Stasiun Tandes').addTo(staAktif);
+const staAktif_kda = L.marker([-7.25104027515005, 112.65817770822828]).bindPopup('Stasiun Kandangan').addTo(staAktif);
+const staAktif_ido = L.marker([-7.176784648037818, 112.66436306589787]).bindPopup('Stasiun Indro').addTo(staAktif);
+const staAktif_bnw = L.marker([-7.233810593698186, 112.61523538124013]).bindPopup('Stasiun Benowo').addTo(staAktif);
+const staAktif_cme = L.marker([-7.224149006786186, 112.57064417939259]).bindPopup('Stasiun Cerme').addTo(staAktif);
+const staAktif_dd = L.marker([-7.154570796329577, 112.52031287939143]).bindPopup('Stasiun Duduk').addTo(staAktif);
+const staAktif_lmg = L.marker([-7.1122464209963, 112.4201070793908]).bindPopup('Stasiun Lamongan').addTo(staAktif);
+const staAktif_sbn = L.marker([-7.099601782008123, 112.35536219288454]).bindPopup('Stasiun Surabayan').addTo(staAktif);
+const staAktif_pc = L.marker([-7.097489513833277, 112.26989233706124]).bindPopup('Stasiun Pucuk').addTo(staAktif);
+const staAktif_geb = L.marker([-7.101920722949443, 112.22218937939074]).bindPopup('Stasiun Gembong').addTo(staAktif);
+const staAktif_bbt = L.marker([-7.105922546539403, 112.16871490822605]).bindPopup('Stasiun Babat').addTo(staAktif);
+const staAktif_bwo = L.marker([-7.130925784240699, 112.09981145240323]).bindPopup('Stasiun Bowerno').addTo(staAktif);
+const staAktif_srj = L.marker([-7.177381308029633, 112.00153396589795]).bindPopup('Stasiun Sumberejo').addTo(staAktif);
+const staAktif_kps = L.marker([-7.199062396195924, 111.93321799473361]).bindPopup('Stasiun Kapas').addTo(staAktif);
+const staAktif_bj = L.marker([-7.163858705263676, 111.88639926589774]).bindPopup('Stasiun Bojonegoro').addTo(staAktif);
+const staAktif_kit = L.marker([-7.13144288450464, 111.76729525055569]).bindPopup('Stasiun Kalitidu').addTo(staAktif);
+const staAktif_tbo = L.marker([-7.171889178777146, 111.6542548677453]).bindPopup('Stasiun Tobo').addTo(staAktif);
 
 
 const staNonaktif = L.layerGroup();
-const staNonaktif_gng = L.marker([-7.584716676572663, 112.72460973591234], {icon: redIcon}).bindPopup('Ex Stasiun Gununggangsir(GNG)').addTo(staNonaktif);
+const staNonaktif_gng = L.marker([-7.584716676572663, 112.72460973591234], {icon: redIcon}).bindPopup('Ex Stasiun Gununggangsir').addTo(staNonaktif);
+const staNonaktif_pbn = L.marker([-7.461393739798361, 112.5643338151328], {icon: redIcon}).bindPopup('Ex Stasiun Prambon').addTo(staNonaktif);
+const staNonaktif_sur = L.marker([-7.179661322155987, 112.545114052404], {icon: redIcon}).bindPopup('Ex Stasiun Sumari').addTo(staNonaktif);
+const staNonaktif_pandanan = L.marker([-7.130842724477226, 112.48133886931714], {icon: redIcon}).bindPopup('Ex Shelter Pandanan').addTo(staNonaktif);
+const staNonaktif_kruwul = L.marker([-7.101007035881965, 112.3703047780907], {icon: redIcon}).bindPopup('Ex Shelter Kruwul').addTo(staNonaktif);
+const staNonaktif_slr = L.marker([-7.093375884843318, 112.32171765240264], {icon: redIcon}).bindPopup('Ex Stasiun Sumlaran').addTo(staNonaktif);
+const staNonaktif_syo = L.marker([-7.163666726398174, 112.0438668370623], {icon: redIcon}).bindPopup('Ex Shelter Sroyo').addTo(staNonaktif);
+const staNonaktif_bsl = L.marker([-7.469259373306871, 112.48388956959967], {icon: redIcon}).bindPopup('Ex Shelter Bangsal').addTo(staNonaktif);
+const staNonaktif_mgr = L.marker([-7.314114296347227, 112.73459826590003], {icon: redIcon}).bindPopup('Ex Shelter Margorejo').addTo(staNonaktif);
+const staNonaktif_jms = L.marker([-7.329193404501409, 112.73142016774763], {icon: redIcon}).bindPopup('Ex Shelter Jemursari').addTo(staNonaktif);
+const staNonaktif_ktl = L.marker([-7.340186197158462, 112.72953325055909], {icon: redIcon}).bindPopup('Ex Shelter Kertomenanggal').addTo(staNonaktif);
+const staNonaktif_stp = L.marker([-7.368289056019354, 112.72927216774849], {icon: redIcon}).bindPopup('Ex Shelter Sawotratap').addTo(staNonaktif);
+const staNonaktif_bjk = L.marker([-7.411926912936675, 112.72614591007833], {icon: redIcon}).bindPopup('Ex Shelter Banjarkemantren').addTo(staNonaktif);
+const staNonaktif_bdr = L.marker([-7.427529132806169, 112.72298299473711], {icon: redIcon}).bindPopup('Ex Shelter Buduran').addTo(staNonaktif);
+const staNonaktif_pwj = L.marker([-7.44210243548792, 112.71525464944723], {icon: redIcon}).bindPopup('Ex Shelter Pagerwojo').addTo(staNonaktif);
 
 // const mbAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
 // const mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
